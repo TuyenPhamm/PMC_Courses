0b69871dd5917007b4b3cff3c314b6d318968ca0
Tuyen Phamm <phamquangtuyen0704@gmail.com>
2023-07-27 21:20:22 +0700

    pushcode

--- src/dreampage/dreampage.jsx
+++ src/dreampage/dreampage.jsx
@@ -1,79 +1,28 @@
 import React, { useState } from 'react';
-import logo from './image/ảnh_Viber_2023-07-24_14-04-37-905.png';
-import background from './image/Section.png'
-import background2 from './image/Section 2.png'
-import link from './image/Link.png';
-import stargreen from './image/.png';
-import staryealow from './image/yellow.png';
-import right from './image/right.png';
-import one from './image/count-one.svg.png';
-import two from './image/count-two.svg.png';
-import three from './image/count-three.svg.png';
-import four from './image/count-four.svg.png';
-import time from './image/time.png'
-import time2 from './image/time2.png'
-
-import category1 from './image/category-1.svg.png';
-import category2 from './image/category-2.svg.png';
-import category3 from './image/category-3.svg.png';
-import category4 from './image/category-4.png';
-import category5 from './image/category-5.svg.png';
-import category6 from './image/category-6.svg.png';
-import category7 from './image/category-7.svg.png';
-import category8 from './image/category-8.svg.png';
-
-import couser1 from './image/Link ⏵ courses-01.jpg.png'
-import couser2 from './image/Link ⏵ courses-02.jpg.png'
-import couser3 from './image/Link ⏵ courses-03.jpg.png'
-import couser4 from './image/Link ⏵ courses-04.jpg.png'
-import couser5 from './image/Link ⏵ courses-05.jpg.png'
-import couser6 from './image/Link ⏵ courses-06.jpg.png'
-
-import skil1 from './image/skil-01.png.png'
-
-import skill1 from './image/skills-01.jpg.png'
-import skill2 from './image/skills-02.jpg.png'
-import skill3 from './image/skills-03.jpg.png'
-import skill1b from './image/skillsb-01.jpg (1).png'
-import skill2b from './image/skillsb-02.jpg (1).png'
-import skill3b from './image/skillsb-03.jpg (1).png'
-
-import linkcouser1 from './image/Link ⏵ skill-01.jpg.png'
-import linkcouser2 from './image/Link ⏵ skill-02.jpg.png'
-import linkcouser3 from './image/Link ⏵ skill-03.jpg.png'
-import linkcouser4 from './image/Link ⏵ skill-04.jpg.png'
-
-import user from './image/map-user.png.png'
-import feature from './image/div.featured-img-1.png'
-
-import join1 from './image/span.joing1-icon.png'
-import join2 from './image/span.joing2-icon.png'
-import join3 from './image/span.joing3-icon.png'
-import join4 from './image/span.joing4-icon.png'
-
-import intructor1 from './image/div.instructors1-img.png'
-import intructor2 from './image/div.instructors2-img.png'
-import intructor3 from './image/div.instructors3-img.png'
-import intructor4 from './image/div.instructors4-img.png'
-import student from './image/student.png'
-import student2 from './image/student2.png'
-
-import review from './image/reviews-img-two.png.png'
-
-import lead1 from './image/lead-01.png.png'
-import lead2 from './image/lead-02.png.png'
-import lead3 from './image/lead-03.png.png'
-import lead4 from './image/lead-04.png.png'
-import lead5 from './image/lead-05.png.png'
-import lead6 from './image/lead-06.png.png'
-
-import blog1 from './image/Link ⏵ blog-01.jpg.png'
-import blog2 from './image/Link ⏵ blog-02.jpg.png'
-import blog3 from './image/Link ⏵ blog-03.jpg.png'
-import day from './image/day.png'
-import round from './image/round.png'
-import close from './image/close.png'
-import down from './image/down.png'
+import  {
+    logo,
+    background,
+    link,
+    stargreen,staryealow,
+    right,
+    one,two,three,four,
+    time,time2,
+    category1,category2,category3,category4,category5,category6,category7,category8,
+    couser1,couser2,couser3,couser4,couser5,couser6,
+    skil1,skill1,skill2,skill3,
+    skill1b,skill2b,skill3b,
+    linkcouser1,linkcouser2,linkcouser3,linkcouser4,
+    user,feature,
+    join1,join2,join3,join4,
+    intructor1,intructor2,intructor3,intructor4,
+    student,student2,
+    review,
+    lead1,lead2,lead3,lead4,lead5,lead6,
+    day,
+    round,
+    close,
+    down,
+    blog1,blog2,blog3} from './image.jsx'
 
 const DreamPage = () => {
     const [bool, setBool] = useState(false);
